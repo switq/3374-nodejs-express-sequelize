4aa80c90a9c1ebb8f4887a2e37250bc01d0ade55
swit <switchaep@gmail.com>
2025-01-09 17:01:21 -0300

    other routes

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,4 +1,4 @@
-const dataSource = require('../models')
+const dataSource = require('../models');
 
 class Services {
     constructor(nomeDoModel) {
@@ -9,12 +9,26 @@ class Services {
         return dataSource[this.model].findAll();
     }
 
+    async pegaUmRegistroPorId(id) {
+      return dataSource[this.model].findByPk(id);
+    }
+
+    async criaRegistro(dadosDoRegistro) {
+      return dataSource[this.model].create(dadosDoRegistro);
+    }
+
     async atualizaRegistro(dadosAtualizados, id) {
-        const listaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+        const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
             where: { id: id }
         });
+        if (listadeRegistrosAtualizados[0] === 0) {
+            return false;
+        }
+        return true;
+    }
 
-        return listaDeRegistrosAtualizados[0] !== 0;
+    async excluiRegistro(id) {
+      return dataSource[this.model].destroy({ where: { id: id } });
     }
 }
 
